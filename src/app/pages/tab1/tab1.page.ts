b71b08b6865e35bfa2b419a36fe96e0099b2595d
Juan Arias <jarias.vis@gmail.com>
2019-05-05 21:24:56 -0500

    Se implementa boton para eliminar tareas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from '../../models/lista.model';
 
 @Component({
   selector: 'app-tab1',
@@ -47,5 +48,9 @@ export class Tab1Page {
     });
 
     alert.present();
+  }
+
+  listaSeleccionada( lista: Lista ) {
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`);
   }
 }
