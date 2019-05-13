d756aa91ee87c9ec30495a4416a9a430cda616c9
Juan Arias <jarias.vis@gmail.com>
2019-05-13 07:13:47 -0500

    Se crea filtro para listar las pendientes y terminadas

---
+++ src/app/pipes/pipes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FiltroCompletadoPipe } from './filtro-completado.pipe';
+
+@NgModule({
+  declarations: [FiltroCompletadoPipe],
+  exports: [FiltroCompletadoPipe]
+})
+export class PipesModule { }
